050884f49d80582bde3e6e2f2d2c3ed48a986b7b
Andy Chen <andycandomore@gmail.com>
2019-08-18 23:31:54 -0700

    handle null content-type header

--- index.js
+++ index.js
@@ -6,36 +6,37 @@ const qs = require('./querystring')
 
 const bodyParser = (req, res, next) => {
   if (req.method.toString() === RequestMethods.PUT || req.method.toString() === RequestMethods.POST) {
-    if (req.headers['content-type'].includes(FORM_URLENCODED)) {
+    const REQ_HEADER_CONTENT_TYPE = req.headers['content-type']
+    if(!REQ_HEADER_CONTENT_TYPE) return next(new Error('cannot parse body data of null'))
+    if (REQ_HEADER_CONTENT_TYPE.includes(FORM_URLENCODED)) {
       let body = ''
       req.on('data', chunk => {
         body += chunk.toString()
       })
       req.on('end', () => {
         req.body = qs.parse(body)
-        next()
+        return next()
       })
-    } else if (req.headers['content-type'].includes(FORM_JSON)) {
+    } else if (REQ_HEADER_CONTENT_TYPE.includes(FORM_JSON)) {
       let body = ''
       req.on('data', chunk => {
         body += chunk.toString()
       })
       req.on('end', () => {
         req.body = JSON.parse(body)
-        next()
+        return next()
       })
-    } else if (req.headers['content-type'].includes(FORM_DATA)) {
-      next(`Doesn't support ${FORM_DATA} yet.`)
-    } else {
-      let body = ''
-      req.on('data', chunk => {
-        body += chunk.toString()
-      })
-      req.on('end', () => {
-        req.body = JSON.parse(body)
-        next()
-      })
+    } else if (REQ_HEADER_CONTENT_TYPE.includes(FORM_DATA)) {
+      return next(`Doesn't support ${FORM_DATA} yet.`)
     }
+    let body = ''
+    req.on('data', chunk => {
+      body += chunk.toString()
+    })
+    req.on('end', () => {
+      req.body = JSON.parse(body)
+      return next()
+    })
   }else{
     next();
   }
